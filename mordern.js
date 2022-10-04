fb5333ebb950b22da2eb3dd82e019ad67a4a1133
jisha66 <mydreamplace2020@gmail.com>
2022-10-04 22:26:59 +0600

    second page

--- mordern.js
+++ mordern.js
@@ -5,6 +5,31 @@
 // let num = () => {
 //     return 10;
 // }
-let num = (a, b) => a + b;
+// let num = (a, b) => a + b;
 
-console.log(num(20, 10));
+// console.log(num(20, 10));
+
+// Es6 ger arrow function
+// var javascript = {
+//     name: "javaScript",
+//     libraries: ["React", "Angular", "Vue"],
+//     printLibraries: function () {
+//         this.libraries.forEach(function (a) {
+//             console.log(`${this.name} loves ${a}`);
+//         });
+//     },
+// };
+
+// javaScript.printLibraries();
+
+var javaScript = {
+    name: "javaScript",
+    libraries: ["React", "Angular", "Vue"],
+    printLibraries: function () {
+        this.libraries.forEach(function (a) {
+            console.log(`${this.name} loves ${a}`);
+        });
+    },
+};
+
+javaScript.printLibraries();
